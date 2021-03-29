38458aa09859d6843203c42d955879c198dc4950
abdulsultans <ideasimperial@gmail.com>
2021-03-29 06:29:03 +0100

    first code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddStudent from "./components/AddStudent";
+import Students from "./components/Students";
+
+function App() {
+  return (
+    <div className="App">
+      <AddStudent />
+      <Students />   
+    </div>
+  );
+}
+
+export default App;
