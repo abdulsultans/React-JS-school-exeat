07e8565fba6a2890258221dd20e5621354255a6a
Abdul Sultan <78996729+abdulsultans@users.noreply.github.com>
2021-06-17 11:17:16 +0000

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
         <footer className="footer">
 
             <h4>Abdul Sultan College of Technology | &copy; Copyright All Rights Reserved - 2021</h4>
+            <h5>Powered by <a href="https://247hitz.com/">247Hitz Media</a></h5>
             
         </footer>
     )
